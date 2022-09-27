0d0315ee2b41f4536e6297c54b12542bef916f88
Mohamed Ait Lahcen <mohamedaitlahcen@gmail.com>
2022-09-27 19:00:08 +0100

    create AccountMapper

---
+++ bank-account-service/src/main/java/org/sid/bankaccountservice/mappers/AccountMapper.java
@@ -0,0 +1,17 @@
+package org.sid.bankaccountservice.mappers;
+
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.sid.bankaccountservice.dtos.BankAccountRequestDTO;
+import org.sid.bankaccountservice.dtos.BankAccountResponseDTO;
+import org.sid.bankaccountservice.entities.BankAccount;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountMapper {
+    public BankAccountResponseDTO fromBankAccount(BankAccount bankAccount){
+        BankAccountResponseDTO bankAccountResponseDTO=new BankAccountResponseDTO();
+        BeanUtils.copyProperties(bankAccount,bankAccountResponseDTO);
+        return bankAccountResponseDTO;
+    }
+}
