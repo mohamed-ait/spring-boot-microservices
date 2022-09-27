223efbdecd2aeb7e2e1c3a6b231a5f6b95e8d369
Mohamed Ait Lahcen <mohamedaitlahcen@gmail.com>
2022-09-27 14:37:43 +0100

    create method to update a bankAccount

--- bank-account-service/src/main/java/org/sid/bankaccountservice/web/AccountRestController.java
+++ bank-account-service/src/main/java/org/sid/bankaccountservice/web/AccountRestController.java
@@ -24,5 +24,14 @@ public BankAccount bankAccount(@PathVariable String id ){
     public BankAccount save( @RequestBody BankAccount ba){
         return  bar.save(ba);
 }
+    @PutMapping("/bankAccounts/{id}")
+    public BankAccount update(@PathVariable String id , @RequestBody BankAccount ba){
+        BankAccount  account = bar.findById(id).orElseThrow();
+       if(ba.getBalance()!=null) account.setBalance(ba.getBalance());
+       if(ba.getType()!=null) account.setType(ba.getType());
+        if(ba.getCreatedAt()!=null)  account.setCurrency(ba.getCurrency());
+        if(ba.getType()!=null)  account.setCreatedAt(ba.getCreatedAt());
+        return  bar.save(account);
+    }
 
 }
