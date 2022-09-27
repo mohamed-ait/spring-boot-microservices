91037ea77d510b94ccfd545af8e14896c946cd73
Mohamed Ait Lahcen <mohamedaitlahcen@gmail.com>
2022-09-27 18:27:43 +0100

    create projection p1

--- bank-account-service/src/main/java/org/sid/bankaccountservice/repositories/BankAccountRepository.java
+++ bank-account-service/src/main/java/org/sid/bankaccountservice/repositories/BankAccountRepository.java
@@ -1,9 +1,13 @@
 package org.sid.bankaccountservice.repositories;
 
 import org.sid.bankaccountservice.entities.BankAccount;
+import org.sid.bankaccountservice.enums.AccountType;
 import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
+    public List<BankAccount> findByType(AccountType type);
 
 }
